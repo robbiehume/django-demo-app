3ac6ecd4d96761df0b0679051920998da71898d5
Robbie Hume <Robert.D.Hume@ugov.gov>
2025-02-28 20:24:50 +0000

    Added ToDoList and Item database models

---
+++ mysite/main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('view1/', views.view1, name='view 1')
+]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(response):
+    return HttpResponse('<h1>hello world</h1>')
+
+
+def view1(response):
+    return HttpResponse('<h1>view 1!</h1>')
