bb67b02a0abbe84fd1394c0aa25b9a0386ad3622
Robbie Hume <Robert.D.Hume@ugov.gov>
2025-03-03 15:49:39 +0000

    Added templates

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('view1/', views.view1, name='view 1')
+    path('', views.home, name='home'),
+    path('<int:id>', views.index, name='index'),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 
-def index(response):
-    return HttpResponse('<h1>hello world</h1>')
+def home(response):
+    return render(response, 'main/home.html')
 
 
-def view1(response):
-    return HttpResponse('<h1>view 1!</h1>')
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, 'main/base.html')
+
+
